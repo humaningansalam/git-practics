33664c3a55e10c4717f7a45f34068ca7f8a66bb1
sh9634 <sanghun9634@gmail.com>
2023-05-01 11:24:30 +0900

    feat: Define print(hello)
    add Define print(hello)

--- main.py
+++ main.py
@@ -1,2 +1,19 @@
 #Test python env
-print('hello')
+
+def print_hello():
+    animals = ['dogs', 'cat', 'hamster']
+    foods = [
+            'Spagetti',
+            'Pizza'
+            ] # w/p trailing comma
+    names = [
+            'John',
+            'Jane',
+            'Gil-dong',
+            ] # w/ trailing comma
+    for f_name in names:
+        print(f'hello, {f_name}')
+
+if __name__ == '__main__':
+
+    print('hello'))
